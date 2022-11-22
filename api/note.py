cbe4d02fb9669082a85fe83c2e034308b2d78f3b
Alternmill <vitalik0969870505@gmail.com>
2022-11-22 16:07:37 +0200

    Fixed responces

--- api/note.py
+++ api/note.py
@@ -5,6 +5,7 @@ from sqlalchemy.sql import func
 from db import *
 from schemas import *
 from models import *
+from api.errors import StatusResponse
 
 note = Blueprint('note', __name__, url_prefix='/note')
 
@@ -15,11 +16,17 @@ def note_create():
     try:
         new_note = NoteCreatingSchema().load(request.json)
     except ValidationError:
-        return jsonify({'Error': 'Invalid input for NoteCreate'}), 400
+        return StatusResponse(response= 'Error : Invalid input for NoteCreate', code = 400) 
+
+    
+    user = db.query(User).filter(User.idUser == new_note['ownerId']).first()
+    if user is None:
+        return 
+
 
     note = db.query(Note).filter(Note.idNote == new_note['id']).first()
     if note is not None:
-        return jsonify({'Error': 'Invalid input for NoteCreate, id already exists'}), 400
+        return StatusResponse(response= 'Error : Invalid input for NoteCreate, id already exists', code = 400) 
 
     note = Note(idNote = new_note['id'],ownerId = new_note['ownerId'],title = new_note['title'],
         isPublic = new_note['isPublic'], text = new_note['text'],dateOfEditing = func.now() 
@@ -42,7 +49,8 @@ def note_create():
 
     db.add(editnote)
     db.commit()
-    return jsonify('Successful creation of a note'),200
+    
+    return StatusResponse(response= 'Successful creation of a note', code = 200) 
 
 @note.route('/<int:id>', methods=['GET'])
 def note_get(id):
@@ -51,8 +59,8 @@ def note_get(id):
     note = db.query(Note).filter(Note.idNote == id).first()
 
     if note is None:
-        return jsonify('Error, no such note'),404
-
+        return StatusResponse(response= 'Error, no such note id', code = 404) 
+    
     note_info = NoteGetSchemaWithAuthors().dump(note)
 
     authors = db.query(EditNote).filter(EditNote.idNote == id).all()
@@ -71,7 +79,7 @@ def note_get(id):
 
     note_info['tags']=tgs
 
-    return jsonify(note_info),200
+    return StatusResponse(response= note_info, code = 200) 
 
 
 @note.route('/<int:id>', methods=['PUT'])
@@ -81,17 +89,17 @@ def note_update(id):
     note = db.query(Note).filter(Note.idNote == id).first()
 
     if note is None:
-        return jsonify('Error, no such note'),404
+        return StatusResponse(response= 'Error, no such note id', code = 404) 
     try:
         new_note = NoteCreatingSchema().load(request.json)
     except ValidationError:
-        return jsonify({'Error': 'Invalid input for NoteCreate'}), 400
+        return StatusResponse(response= 'Error, invalid input', code = 400) 
 
     for tag in new_note['tags']:
         print(tag)
         ch = db.query(Tag).filter(Tag.idTag == tag).first()
         if ch is None:
-            return jsonify('No such tags exist'),400
+            return StatusResponse(response= 'Error, no such tag id', code = 404) 
 
     note.title = new_note['title']
     note.isPublic = new_note['isPublic']
@@ -111,7 +119,8 @@ def note_update(id):
        
     db.commit()
 
-    return jsonify("Update successful"),200
+    return StatusResponse(response= 'Update successful', code = 200) 
+
 
 @note.route('/<int:id>', methods=['DELETE'])
 def note_delete(id):
@@ -120,7 +129,8 @@ def note_delete(id):
     note = db.query(Note).filter(Note.idNote == id).first()
 
     if note is None:
-        return jsonify('Error, no such note'),404
+        return StatusResponse(response= 'Error, no such note id', code = 404) 
+
     
     db.query(Tags).filter(Tags.idNote == id).delete()
     db.query(EditNote).filter(EditNote.idNote == id).delete()
@@ -130,7 +140,8 @@ def note_delete(id):
 
     db.commit()
 
-    return jsonify("Delete successful"),200
+    return StatusResponse(response= 'Delete successful', code = 200) 
+
 
 @note.route('/allowed', methods=['POST'])
 def note_allow_change():
@@ -139,19 +150,19 @@ def note_allow_change():
     try:
         new_allow = AllowSchema().load(request.json)
     except ValidationError as err:
-        return jsonify(err), 400
+        return StatusResponse(response= err, code = 400) 
 
 
     allow = db.query(EditNote).filter(EditNote.idNote == new_allow['idNote'], EditNote.idUser == new_allow['idUser']).first()
 
     if allow is not None:
-        return jsonify("User already has access"),200
+        return StatusResponse("User already has access",200)
 
     allow = EditNote(idNote = new_allow['idNote'],idUser = new_allow['idUser'])
     db.add(allow)
     db.commit()
 
-    return jsonify("User now has access to modify"),200
+    return StatusResponse("User now has access to modify",200)
 
 @note.route('/allowed', methods=['DELETE'])
 def note_disallow_change():
@@ -160,19 +171,20 @@ def note_disallow_change():
     try:
         old_allow = AllowSchema().load(request.json)
     except ValidationError as err:
-        return jsonify(err), 400
+         return StatusResponse(response= err, code = 400) 
+
 
 
     allow = db.query(EditNote).filter(EditNote.idNote == old_allow['idNote'], EditNote.idUser == old_allow['idUser']).first()
 
     if allow is None:
-        return jsonify("User already does not have access"),200
+        return StatusResponse("User already has access",200)
     
     note = db.query(Note).filter(Note.idNote == old_allow['idNote']).first()
     if note.ownerId == old_allow['idUser']:
-        return jsonify("User is an owner of a note"),400
-
+        return StatusResponse("User is an owner of a note",400)
+      
     db.query(EditNote).filter(EditNote.idNote == old_allow['idNote'], EditNote.idUser == old_allow['idUser']).delete()
     db.commit()
 
-    return jsonify("User now does not have access to modify"),200
+    return StatusResponse("User now does not have access to modify",200)
--- api/tag.py
+++ api/tag.py
@@ -5,17 +5,24 @@ from sqlalchemy import desc
 from db import *
 from schemas import *
 from models import *
+from api.errors import StatusResponse
 
 tag = Blueprint('tag', __name__, url_prefix='/tag')
 
 @tag.route('/', methods=['POST'])
 def tag_create():
     db = get_db()
-    
+
     try:
         new_tag = TagSchema().load(request.json)
     except ValidationError:
-        return jsonify({'Error': 'Invalid input for TagCreate'}), 400
+        return StatusResponse(response= 'Error : Invalid input for TagCreate', code = 400) 
+
+    check = db.query(Tag).filter(Tag.idTag == new_tag['idTag']).all()
+
+    if check is not None:
+        return StatusResponse(response= 'Error : tag with this id already exists', code = 400) 
+
 
     tag = Tag(idTag = new_tag['idTag'],text = new_tag['text'])
 
@@ -23,7 +30,7 @@ def tag_create():
 
     db.commit()
 
-    return jsonify('successful creation of a tag')
+    return StatusResponse(response= 'Successful creation of a tag', code = 200) 
 
 @tag.route('/<int:id>', methods=['GET'])
 def tag_get(id):
@@ -32,11 +39,11 @@ def tag_get(id):
     tag = db.query(Tag).filter(Tag.idTag == id).first()
 
     if tag is None:
-        return jsonify('Error, no such tag'),404
+        return StatusResponse('Error, no such tag', code = 404) 
 
     tag_info = TagSchema().dump(tag)
 
-    return jsonify(tag_info)
+    return StatusResponse(response= tag_info, code = 200) 
    
 
 @tag.route('/<int:id>', methods=['PUT'])
@@ -46,18 +53,18 @@ def tag_update(id):
     tag = db.query(Tag).filter(Tag.idTag == id).first()
 
     if tag is None:
-        return jsonify('Error, no such tag'),404
+        return StatusResponse('Error, no such tag', code = 404) 
 
     try:
         new_tag = TagUpdateSchema().load(request.json)
     except ValidationError:
-        return jsonify({'Error': 'Invalid input for TagUpdate'}), 400
+       return StatusResponse(response= 'Error : Invalid input for TagUpdate', code = 400) 
 
     tag.text = new_tag['text']
 
     db.commit()
 
-    return jsonify('Update successful'),200
+    return StatusResponse(response= new_tag, code = 200) 
 
 
 @tag.route('/<int:id>', methods=['DELETE'])
@@ -67,8 +74,7 @@ def tag_delete(id):
     tag = db.query(Tag).filter(Tag.idTag == id).first()
 
     if tag is None:
-        return jsonify('Error, no such tag'),404
-   
+        return StatusResponse('Error, no such tag', code = 404)
     ctags = db.query(Tags).filter(Tags.idTag == id).all()
 
     for ctag in ctags:
@@ -77,4 +83,4 @@ def tag_delete(id):
     db.query(Tag).filter(Tag.idTag == id).delete()
 
     db.commit()
-    return jsonify('Delete successful'),200
+    return StatusResponse(response= 'Delete successful', code = 200) 
